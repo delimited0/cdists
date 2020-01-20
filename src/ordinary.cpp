f913b4d7da8fa1d8ad235a4b7a4f7a62067b1472
hillarykoch <hillary.koch01@gmail.com>
2020-01-19 19:02:55 -0500

    update RNG for ordinary functions

--- src/ordinary.cpp
+++ src/ordinary.cpp
@@ -1,14 +1,8 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 
-// [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::depends(BH)]]
-// [[Rcpp::plugins(cpp11)]]
 #include <RcppArmadillo.h>
-#include <random>
-#include <boost/math/distributions/normal.hpp>
-#include <boost/math/distributions/chi_squared.hpp>
+//#include <iostream>
 
-using namespace boost::math;
 using namespace Rcpp;
 using namespace RcppArmadillo;
 
@@ -16,16 +10,12 @@ inline int randWrapper(const int n) { return floor(unif_rand()*n); }
 
 // [[Rcpp::export]]
 arma::cube cr_ordinary_wish(int n, arma::mat Sigma, double nu, int npairs) {
-    std::default_random_engine generator{(unsigned) randWrapper(INT_MAX)};
     int d = Sigma.n_rows;
     int m;
     int p;
     arma::mat U = chol(Sigma, "upper");
     arma::mat A(d, d, arma::fill::zeros);
     arma::cube out(d, d, n, arma::fill::zeros);
-    
-    normal standard_normal;
-    std::uniform_real_distribution<double> standard_uniform(0.0, 1.0);
     
     arma::rowvec dp;
     arma::mat dimpairs(npairs, 2, arma::fill::none);
@@ -40,14 +30,13 @@ arma::cube cr_ordinary_wish(int n, arma::mat Sigma, double nu, int npairs) {
     
     for(auto iteration = 0; iteration < n; iteration++) {
         for(auto i = 0; i < d; i++) {
-            chi_squared chidist(nu-i);
-            A(i,i) = sqrt(quantile(chidist, standard_uniform(generator)));
+            A(i,i) = sqrt(as<double>(wrap(Rcpp::rchisq(1, nu - i))));
         }
             
         for(auto idx = 0; idx < npairs; idx++) {
             m = dimpairs(idx,0);
             p = dimpairs(idx,1);
-            A(p,m) = quantile(standard_normal, standard_uniform(generator));
+            A(p,m) = as<double>(wrap(Rcpp::qnorm(Rcpp::runif(1), 0.0, 1.0, 1, 0)));
         }
         out.slice(iteration) = (U.t() * A * A.t() * U);
     }
@@ -57,19 +46,14 @@ arma::cube cr_ordinary_wish(int n, arma::mat Sigma, double nu, int npairs) {
 
 // [[Rcpp::export]]
 arma::mat cr_ordinary_mvn(int n, arma::mat Sigma, arma::colvec mu) {
-    std::default_random_engine generator;
     int d = mu.size();
     arma::mat L = chol(Sigma, "lower");
     arma::mat z(d, n, arma::fill::zeros);
     
-    normal standard_normal;
-    std::uniform_real_distribution<double> standard_uniform(0.0, 1.0);
-
     for(auto iteration = 0; iteration < n; iteration++){
         for(auto i = 0; i < d; i++) {
-            z(i, iteration) = quantile(standard_normal, standard_uniform(generator));
+            z(i, iteration) = as<double>(wrap(Rcpp::qnorm(Rcpp::runif(1), 0.0, 1.0, 1, 0)));
         }
     }
     return((L * z).t());
 }
-
